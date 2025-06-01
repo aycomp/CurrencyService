528f663fe2bbc138279eb4024d9efab3113bb3cc
Ayse Ozdemir <ayse.ozdemir@payten.com>
2025-06-01 11:21:42 +0300

    Add initial project structure and pom.xml

---
+++ src/main/java/com/ao/CurrencyExhange/CurrencyExhangeApplication.java
@@ -0,0 +1,13 @@
+package com.ao.CurrencyExhange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExhangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExhangeApplication.class, args);
+	}
+
+}
